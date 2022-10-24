3dffaa97855b504f36c33fe48ffbac97d251d7a3
mor <mor@DESKTOP-7VQJJUQ>
2022-10-24 17:53:46 +0300

    Add project files.

---
+++ LEC_01 - HW/Car.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LEC_01___HW
+{
+    internal class Car
+    {
+        string _company;
+        int _plate;
+        int _model;
+
+        public string Company
+        {
+            get { return _company; }
+            set { _company = value; }
+        }
+        public int Plate
+        {
+            get { return _plate; }
+            set { _plate = value; }
+        }
+        public int Model
+        {
+            get { return _model; }
+            set
+            {
+                if (value < 1991)
+                    return;
+                _model = value;
+            }
+        }
+        public void printCN()
+        {
+            Console.WriteLine($"Your Car Company Name Is: {Company}");
+        }
+
+        public void printModel()
+        {
+            Console.WriteLine($"Your Car Model Is: {Model}");
+        }
+
+        public void printLP()
+        {
+            Console.WriteLine($"Your Car License Plate Is: {Plate}");
+        }
+    }
+}
---
+++ LEC_01 - HW/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+/******************************************************************************
+*                              שיעורי בית
+                        ------------------------
+* A) private ליצור מחלקה חדשה של רכב ולהגדיר 3 משתנים מסוג 
+* 1) שם החברה של הרכב
+* 2) מספר הרכב
+* 3) שנת ייצור
+
+* B) לכל אחד מהם get-ו set מגדירים
+* נבצע בדיקה של שנת הייצור
+* במידה והשנת ייצור קטנה מ-1991
+* לא לעשות השמה
+
+* C) ניצור פונקציה שמדפיסה את שם החברה של הרכב
+*    פונקציה שמדפיסה את מספר הרכב
+*    פונקציה שמדפיסה את שנת הייצור
+*******************************************************************************/
+
+using LEC_01___HW;
+
+while (true)
+{
+  Car Ex = new Car();
+
+  Console.WriteLine("\nPlease Enter Your Car Company Name:\n");
+  Ex.Company = Console.ReadLine();
+
+
+  Console.WriteLine("\nPlease Enter Your Car License Plate:\n");
+  Ex.Plate = int.Parse(Console.ReadLine());
+
+
+  Console.WriteLine("\nPlease Enter Your Car Model Year:\n");
+  Ex.Model = int.Parse(Console.ReadLine());
+
+  Console.WriteLine("\nYour Car Details:\n-------------------");
+  Ex.printCN();
+  Ex.printLP();
+  Ex.printModel();
+}
